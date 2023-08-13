b4128be9054384975fdc39bbef5760959e74ce28
tushar-asodariya <tusharasodariya1@gmail.com>
2023-08-13 17:17:29 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/pokemon/app/pokemon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pokemon.app.pokemon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/color_constants.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+
+  static Color snowColor = fromHex("#F8FAF9");
+
+
+
+
+
+
+
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+}
