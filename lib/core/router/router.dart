cc85387e427ddec829fde4e12a2edc90bf9e8e44
tushar-asodariya <tusharasodariya1@gmail.com>
2023-08-15 10:19:07 +0530

    pokemon list domain usecases added

---
+++ lib/core/router/router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pokemon/core/constants/route_constants.dart';
+import 'package:pokemon/features/all_pokemons/screens/pokemon_list_screen.dart';
+
+class AppRouter{
+
+   Route? onGenerateRoute(RouteSettings settings) {
+    switch(settings.name){
+      case Routes.pokemonListScreen:
+        return MaterialPageRoute(builder: (_) => PokemonListScreen());
+      default : 
+        return null;
+    }
+   }
+}
---
+++ lib/features/all_pokemons/screens/pokemon_list_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PokemonListScreen extends StatefulWidget {
+  const PokemonListScreen({super.key});
+
+  @override
+  State<PokemonListScreen> createState() => _PokemonListScreenState();
+}
+
+class _PokemonListScreenState extends State<PokemonListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
